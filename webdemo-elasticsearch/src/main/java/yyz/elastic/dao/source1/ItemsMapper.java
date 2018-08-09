2be65187bea8faba31edb9ad18f3d952a14ffbcd
yaoyizhou1 <yaoyizhou@jd.com>
2018-08-09 14:16:57 +0800

    初始化

--- webdemo-elasticsearch/src/main/java/yyz/elastic/dao/source1/ItemsMapper.java
+++ webdemo-elasticsearch/src/main/java/yyz/elastic/dao/source1/ItemsMapper.java
@@ -5,17 +5,7 @@ import yyz.elastic.domain.Items;
 import java.util.List;
 
 public interface ItemsMapper {
-//    int deleteByPrimaryKey(Integer id);
 
     List<Items> selectAll();
 
-//    int insert(Items record);
-
-//    int insertSelective(Items record);
-
-//    Items selectByPrimaryKey(Integer id);
-
-//    int updateByPrimaryKeySelective(Items record);
-
-//    int updateByPrimaryKey(Items record);
 }
